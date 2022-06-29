a6c8d41e0c772bda3df178af114eb3e8716f8f16
Thi Nguyen <nguyenthisc1@gmai.com>
2022-06-29 15:01:55 +0700

    add assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
+import 'package:chat_app/app/utils/error.screen.dart';
+import 'package:chat_app/app/utils/loading.screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app/utils/splash.screen.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return ErrorScreen();
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return FutureBuilder(
+              future: Future.delayed(Duration(seconds: 3)),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return GetMaterialApp(
+                    title: "Chat App",
+                    initialRoute: AppPages.INITIAL,
+                    getPages: AppPages.routes,
+                  );
+                }
+                return SplashScreen();
+              });
+        }
+        return LoadingScreen();
+      },
+    );
+  }
 }
